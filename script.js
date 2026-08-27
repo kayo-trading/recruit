504dbca3bb29885f27918f643479ee3d342210fb
tsuzurajun-kayotrading <tsuzurajun222@gmail.com>
2026-08-27 23:45:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll('.recruit-accordion-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+      const item = btn.parentElement;
+      const content = btn.nextElementSibling;
+
+      item.classList.toggle('active');
+
+      if (item.classList.contains('active')) {
+        content.style.maxHeight = content.scrollHeight + "px";
+      } else {
+        content.style.maxHeight = null;
+      }
+    });
+  });
+});
