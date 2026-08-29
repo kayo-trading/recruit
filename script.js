93c28f8776f97a8aecef69aa884d2d07dbcd2e9f
kayo-trading <tsuzurajun222@gmail.com>
2026-08-29 18:39:56 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ document.querySelectorAll('.recruit-accordion-btn').forEach(btn => {
   btn.addEventListener('click', () => {
     const item = btn.parentElement;
     const content = btn.nextElementSibling;
-
     item.classList.toggle('active');
-
     if (item.classList.contains('active')) {
       content.style.maxHeight = content.scrollHeight + "px";
     } else {
