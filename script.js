aceaccb3817db00e09d9e18511d405ac0d97e946
kayo-trading <tsuzurajun222@gmail.com>
2026-08-29 17:52:14 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll('.recruit-accordion-btn').forEach(btn => {
-    btn.addEventListener('click', () => {
-      const item = btn.parentElement;
-      const content = btn.nextElementSibling;
+document.querySelectorAll('.recruit-accordion-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const item = btn.parentElement;
+    const content = btn.nextElementSibling;
 
-      item.classList.toggle('active');
+    item.classList.toggle('active');
 
-      if (item.classList.contains('active')) {
-        content.style.maxHeight = content.scrollHeight + "px";
-      } else {
-        content.style.maxHeight = null;
-      }
-    });
+    if (item.classList.contains('active')) {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } else {
+      content.style.maxHeight = null;
+    }
   });
 });
